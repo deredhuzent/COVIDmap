11aef895b0d1f083b1bd3f3b46d38dc8c904b5dd
Deryana <deredhuzent@gmail.com>
2020-07-22 10:20:36 -0700

    Initial commit

---
+++ app/src/main/java/com/deredhuzent/covidmap/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.deredhuzent.covidmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
