8aca9d75d18b47bcb3d7648ff0c202afd8ce30fa
Deryana <deredhuzent@gmail.com>
2020-07-29 11:58:20 -0700

    recycle view layout for place info basic

--- app/src/main/java/com/deredhuzent/covidmap/MainActivity.kt
+++ app/src/main/java/com/deredhuzent/covidmap/MainActivity.kt
@@ -2,11 +2,28 @@ package com.deredhuzent.covidmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+//extend MainActivity to OnMapReadyCallback
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    lateinit var myMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    //when the map is ready and the communication is done with google, then set myMap
+    override fun onMapReady(gMap: GoogleMap?) {
+
+        //when the map is ready, myMap becomes a google Map so I can use it
+        myMap = gMap!!
     }
 }
